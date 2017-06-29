5596892124509fe291ac9596719b611ed7bf4320
Phil Cooper-King <phil@cooperking.net>
2017-06-29 20:11:23 +0100

    Added nau project.

---
+++ lib/nau/include/nau/renderers/opengl3.h
@@ -0,0 +1,17 @@
+#ifndef OPEN_GL3_INCLUDED_F0DFC029_F493_471A_B85E_B9C512D3ABF9
+#define OPEN_GL3_INCLUDED_F0DFC029_F493_471A_B85E_B9C512D3ABF9
+
+
+#include <nau/nau.h>
+
+
+// ------------------------------------------------------------- [ Lifetime ] --
+
+
+void      nau_gl_init();
+void      nau_gl3_render();
+void      nau_gl_destroy();
+
+
+
+#endif // inc guard
---
+++ lib/nau/src/renderer/opengl3.cpp
@@ -0,0 +1,8 @@
+#include <nau/renderers/opengl3.h>
+
+
+void
+nau_gl3_render(Nau_renderable *renderables, size_t count)
+{
+  
+}
