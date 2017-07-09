b3d7b2e72a862d41803fc0ea282b671f8a43ddfe
Phil Cooper-King <phil@cooperking.net>
2017-07-09 12:18:27 +0100

    new project

---
+++ app/blank/src/main.cpp
@@ -0,0 +1,81 @@
+#include <nil/nil.hpp>
+#include <nil/node.hpp>
+#include <nil/data/data.hpp>
+#include <nil/resource/resource.hpp>
+#include <aspect/aspects.hpp>
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
+#include <lib/logging.hpp>
+#include <lib/string.hpp>
+
+#include <assert.h>
+#include <stddef.h>
+#include <string.h>
+
+
+Nil::Node app;
+Nil::Node camera;
+
+
+void
+app_tick()
+{
+  #ifdef __EMSCRIPTEN__
+  nil_engine.run();
+  #endif
+}
+
+
+int
+main()
+{
+  lib::logging::set_output(lib::logging::out::console);
+  
+  Nil::Engine nil_engine;
+  Nil_ext::load_aspects(nil_engine);
+  
+  // Basic App
+  {
+    app.set_name("App");
+    Nil::Node node = app;
+    
+    Nil::Data::Window win{};
+    constexpr char title[] = "Viewer";
+    strcat(win.title, title);
+    
+    Nil::Data::set(node, win);
+    Nil::Data::set(node, Nil::Data::Mouse{});
+    Nil::Data::set(node, Nil::Data::Keyboard{});
+  }
+  
+  // Camera
+  {
+    camera.set_name("Camera");
+    Nil::Data::Camera cam{};
+    cam.near_plane = 0.1f;
+    cam.far_plane = 1000.f;
+    cam.fov = math::tau() * 0.125;
+    cam.clear_color_buffer = true;
+    cam.clear_depth_buffer = true;
+    
+    Nil::Data::set(camera, cam);
+  }
+  
+
+  /*
+    Run Game
+  */
+  #ifdef __EMSCRIPTEN__
+  emscripten_set_main_loop(app_tick, -1, 1);
+  #else
+  while(nil_engine.run())
+  {
+    app_tick();
+  }
+  #endif
+
+  return 0;
+}
