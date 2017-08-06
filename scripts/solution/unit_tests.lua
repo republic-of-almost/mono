974c98cb7ccd055160672dfa7cfeb8f3ada4ea8c
Phil Cooper-King <phil@cooperking.net>
2017-08-06 02:31:24 +0100

    Seperate unit test only solution

--- scripts/solution/unit_tests.lua
+++ scripts/solution/unit_tests.lua
@@ -5,14 +5,15 @@ dofile("./premake_data.lua")
 dofile("./defaults.lua")
 
 
+-- This is used by the individual projects, but it needs to be defined first.
 projects = {}
 
+proj_matches = os.matchfiles("../../**.project")
 test_matches = os.matchfiles("../../**.test_project")
 
 for i, proj in ipairs(test_matches) do
   dofile(proj)
 end
-
 
 -- Generates the premake code calls.
 make.create_solution(
