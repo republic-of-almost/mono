9f4e8e781c1764392f6489e800b446fc62cf145a
Phil Cooper-King <phil@cooperking.net>
2017-12-07 21:45:48 +0000

    Added AS and repo app

---
+++ app/repo_app/src/main.cpp
@@ -0,0 +1,31 @@
+#include <anglescript/angelscript.h>
+#include <optio/dispatcher.h>
+
+
+void
+anglescript_job(void *)
+{
+}
+
+
+void
+job_new_frame(void*)
+{
+  
+}
+
+
+
+
+int
+main()
+{
+  optio_dispatcher_ctx *dispatcher_ctx;
+  optio_dispatcher_create(&dispatcher_ctx);
+  
+  
+  
+  
+
+  return 0;
+}
