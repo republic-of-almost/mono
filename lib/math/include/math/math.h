07a2b5997a5be9f41e9dfa1cc698c17ec57e6759
phil0005 <pcooperking@esri.com>
2018-01-16 13:24:01 +0000

    removed old tags, math to c sketch.

---
+++ lib/math/include/math/math.h
@@ -0,0 +1,28 @@
+#ifndef MATH_INCLUDED_
+#define MATH_INCLUDED_
+
+
+/* ------------------------------------------------------------- [ types ] -- */
+
+
+typedef struct float2 { float data[2];  }         float2;
+typedef struct float3 { float data[3];  }         float3;
+typedef struct float4 { float data[4];  }         float4;
+typedef struct mat3   { float data[9];  }         mat3;
+typedef struct mat4   { float data[18]; }         mat4;
+typedef struct aabb   { float3 min; float3 max; } aabb;
+
+
+/* -- [ float2 ] -- */
+
+float       float2_get_x(float2 a);
+float       float2_get_y(float2 a);
+
+float2      float2_add(float2 a, float2 b);
+float2      float2_subtract(float2 a, float2 b);
+float2      float2_multiply(float2 a, float2 b);
+
+/* -- [ float3 ] -- */
+
+
+#endif /* inc guard */
