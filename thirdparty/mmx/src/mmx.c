102be8eeaa442d77092927e85ea041c0b516b004
Phil Cooper-King <phil@cooperking.net>
2018-02-20 21:30:34 +0000

    updated mmx, and added nuklear

--- thirdparty/mmx/src/mmx.c
+++ thirdparty/mmx/src/mmx.c
@@ -1,5 +1,11 @@
 #define JSON_IMPLEMENTATION
 #include <mmx/json.h>
 
+/* lexer impl seems to need as type defined in here for MSVS */
+#include <mmx/web.h>
+
 #define LEXER_IMPLEMENTATION
 #include <mmx/lexer.h>
+
+#define NK_IMPLEMENTATION
+#include <nuklear/nuklear.h>
