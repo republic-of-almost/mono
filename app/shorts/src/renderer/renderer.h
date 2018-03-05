3dba9d231f78fa44a6992f1313a36fe78cd47440
Phil Cooper-King <phil@cooperking.net>
2018-03-05 07:30:16 +0000

    renderer

---
+++ app/shorts/src/renderer/renderer.h
@@ -0,0 +1,12 @@
+#ifndef RENDERER_INCLUDED_B56AE667_C633_42DB_94F9_55BDD820B6CF
+#define RENDERER_INCLUDED_B56AE667_C633_42DB_94F9_55BDD820B6CF
+
+
+struct mesh_renderable
+{
+  volt_vbo_t vbo;
+  volt_ibo_i ibo;
+};
+
+
+#endif /* inc guard */
