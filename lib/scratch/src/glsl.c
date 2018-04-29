fd0cf84480702d747447199aeca167f6efd1b699
Phil Cooper-King <phil@cooperking.net>
2018-04-29 13:50:51 +0100

    Updated shader.

--- lib/scratch/src/glsl.c
+++ lib/scratch/src/glsl.c
@@ -63,12 +63,15 @@ glsl_fullbright_vs()
     "layout(location=2) in vec2 vs_in_texcoord;\n"
 
     "uniform mat4 uni_wvp_mat;\n"
+    "uniform mat4 uni_world_mat;\n"
 
     "out vec2 fs_in_texcoord;\n"
+    "out vec4 fs_in_grid;\n"
 
     "void main() {\n"
     "  fs_in_texcoord = vs_in_texcoord;\n"
     "  gl_Position = uni_wvp_mat * vec4(vs_in_position, 1.0);\n"
+    "  fs_in_grid = uni_world_mat * vec4(vs_in_position, 1.0);\n"
     "}\n";
 
   return vert_src;
@@ -80,7 +83,8 @@ glsl_fullbright_fs()
 {
   const char* frag_src = ""
     "#version 400 core\n"
-
+    
+    "in vec4 fs_in_grid;\n"
     "in vec2 fs_in_texcoord;\n"
 
     "uniform sampler2D samp_diffuse_01;\n"
@@ -89,7 +93,13 @@ glsl_fullbright_fs()
     "out vec4 fs_out_fragcolor;\n"
 
     "void main() {\n"
-    "  fs_out_fragcolor = mix(texture(samp_diffuse_01, fs_in_texcoord), uni_color, uni_color.a);\n"
+    "  float grid_x = abs(fract(fs_in_grid.x - 0.5) - 0.5) / fwidth(fs_in_grid.x);\n"
+    "  float grid_y = abs(fract(fs_in_grid.y - 0.5) - 0.5) / fwidth(fs_in_grid.y);\n"
+    "  float grid_z = abs(fract(fs_in_grid.z - 0.5) - 0.5) / fwidth(fs_in_grid.z);\n"
+
+    "  float line = min(min(grid_x, grid_y), grid_z);\n"
+    "  vec4 base_color = mix(texture(samp_diffuse_01, fs_in_texcoord), uni_color, uni_color.a);\n"
+    "  fs_out_fragcolor = mix(vec4(0.2,0.2,0.3,1), base_color, line);\n"
     "}\n";
 
   return frag_src;
