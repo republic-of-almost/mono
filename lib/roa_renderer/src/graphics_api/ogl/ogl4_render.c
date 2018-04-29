e18f07fb92ae0249780207eccf1629890d2e5087
Phil Cooper-King <phil@cooperking.net>
2018-04-29 13:51:20 +0100

    Changed default color

--- lib/roa_renderer/src/graphics_api/ogl/ogl4_render.c
+++ lib/roa_renderer/src/graphics_api/ogl/ogl4_render.c
@@ -9,6 +9,8 @@ platform_render(roa_renderer_ctx_t ctx)
   glClearColor(1,1,0,1);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+  glEnable(GL_DEPTH_TEST);
+
   GLsizei vp_width  = ctx->settings.device_viewport[0];
   GLsizei vp_height = ctx->settings.device_viewport[1];
 
@@ -35,10 +37,17 @@ platform_render(roa_renderer_ctx_t ctx)
 
     for (j = 0; j < draw_call; ++j)
     {
-      GLint uni_wvp = glGetUniformLocation(ctx->gfx_api.mesh_program, "uni_wvp_mat");
-      glUniformMatrix4fv(uni_wvp, 1, GL_FALSE, rp->draw_calls[j].world_view_projection);
+      float *world_mat = rp->draw_calls[j].world;
 
-      float color[] = {1,0,1,1};
+      GLint uni_world = glGetUniformLocation(ctx->gfx_api.mesh_program, "uni_world_mat");
+      glUniformMatrix4fv(uni_world, 1, GL_FALSE, world_mat);
+
+      float *wvp_mat = rp->draw_calls[j].world_view_projection;
+
+      GLint uni_wvp = glGetUniformLocation(ctx->gfx_api.mesh_program, "uni_wvp_mat");
+      glUniformMatrix4fv(uni_wvp, 1, GL_FALSE, wvp_mat);
+
+      float color[] = {0.8,0.8,0.85,1};
 
       GLint u_color = glGetUniformLocation(ctx->gfx_api.mesh_program, "uni_color");
       glUniform4fv(u_color, 1, color);
