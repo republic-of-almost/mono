404c1642ae9a114d49bb42d0185ac28bc5be2d19
Phil Cooper-King <phil@cooperking.net>
2018-05-01 07:18:09 +0100

    Formatting

--- lib/roa_job/test/unit/main.cpp
+++ lib/roa_job/test/unit/main.cpp
@@ -5,53 +5,50 @@
 
 
 void
-some_work(roa_job_dispatcher_ctx_t ctx, void *arg)
-{
-  /* do nothing */
-}
+some_dummy_work(roa_job_dispatcher_ctx_t, void *) { /* do nothing */ }
 
 
 void
-some_other_work(roa_job_dispatcher_ctx_t ctx, void *arg)
-{
-  /* do nothing */
-}
+some_other_dummy_work(roa_job_dispatcher_ctx_t, void *) { /* do nothing */ }
+
 
 void
-double_submit_crash(roa_job_dispatcher_ctx_t ctx, void *arg)
+double_submit_crash(roa_job_dispatcher_ctx_t ctx, void *)
 {
   roa_job_desc desc{};
-  desc.func = some_work;
+  desc.func = some_dummy_work;
 
   roa_job_submit(ctx, &desc, 1);
 
   roa_job_desc other_desc{};
-  desc.func = some_other_work;
+  desc.func = some_other_dummy_work;
 
   roa_job_submit(ctx, &other_desc, 1);
 }
 
 
 void
-shutdown_failure(roa_job_dispatcher_ctx_t ctx, void *arg)
+shutdown_failure(roa_job_dispatcher_ctx_t ctx, void *)
 {
   roa_job_desc desc{};
-  desc.func = shutdown_failure;
+  desc.func = some_dummy_work;
 
-  roa_job_submit(ctx, &desc, 1);
-  roa_job_submit(ctx, &desc, 1);
   roa_job_submit(ctx, &desc, 1);
   roa_job_submit(ctx, &desc, 1);
 }
 
 
-TEST_CASE("Bug")
+TEST_CASE("Regression Tests")
 {
   roa_job_dispatcher_ctx_t ctx{};
   roa_job_dispatcher_ctx_create(&ctx, ROA_NULL);
 
   SECTION("double submit crash")
   {
+    /*
+      Bug: crash (note: if submit same function twice it doesn't crash)
+    */
+
     roa_job_desc desc{};
     desc.func = double_submit_crash;
 
@@ -63,8 +60,12 @@ TEST_CASE("Bug")
 
   SECTION("shutdown failure")
   {
+    /*
+      Bug: dispatcher shutdown doesn't happen.
+    */
+
     roa_job_desc desc{};
-    desc.func = double_submit_crash;
+    desc.func = shutdown_failure;
 
     roa_job_submit(ctx, &desc, 1);
     roa_job_dispatcher_ctx_run(ctx);
