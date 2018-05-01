8ed115a7a792b890e3139509700f70abde4ead9d
Phil Cooper-King <phil@cooperking.net>
2018-05-01 07:10:58 +0100

    added two bug reproducers

--- lib/roa_job/test/unit/main.cpp
+++ lib/roa_job/test/unit/main.cpp
@@ -1,7 +1,76 @@
+#include <roa_job/roa_job.h>
+
 #define CATCH_CONFIG_MAIN
 #include <catch/catch.hpp>
 
-TEST_CASE("Dummy")
+
+void
+some_work(roa_job_dispatcher_ctx_t ctx, void *arg)
 {
-  REQUIRE(true);
+  /* do nothing */
+}
+
+
+void
+some_other_work(roa_job_dispatcher_ctx_t ctx, void *arg)
+{
+  /* do nothing */
+}
+
+void
+double_submit_crash(roa_job_dispatcher_ctx_t ctx, void *arg)
+{
+  roa_job_desc desc{};
+  desc.func = some_work;
+
+  roa_job_submit(ctx, &desc, 1);
+
+  roa_job_desc other_desc{};
+  desc.func = some_other_work;
+
+  roa_job_submit(ctx, &other_desc, 1);
+}
+
+
+void
+shutdown_failure(roa_job_dispatcher_ctx_t ctx, void *arg)
+{
+  roa_job_desc desc{};
+  desc.func = shutdown_failure;
+
+  roa_job_submit(ctx, &desc, 1);
+  roa_job_submit(ctx, &desc, 1);
+  roa_job_submit(ctx, &desc, 1);
+  roa_job_submit(ctx, &desc, 1);
+}
+
+
+TEST_CASE("Bug")
+{
+  roa_job_dispatcher_ctx_t ctx{};
+  roa_job_dispatcher_ctx_create(&ctx, ROA_NULL);
+
+  SECTION("double submit crash")
+  {
+    roa_job_desc desc{};
+    desc.func = double_submit_crash;
+
+    roa_job_submit(ctx, &desc, 1);
+    roa_job_dispatcher_ctx_run(ctx);
+
+    REQUIRE(true);
+  }
+
+  SECTION("shutdown failure")
+  {
+    roa_job_desc desc{};
+    desc.func = double_submit_crash;
+
+    roa_job_submit(ctx, &desc, 1);
+    roa_job_dispatcher_ctx_run(ctx);
+
+    REQUIRE(true);
+  }
+
+  roa_job_dispatcher_ctx_destroy(&ctx);
 }
