8e538f73f572016223ce85b2aff98b9647c0bc85
Phil Cooper-King <phil@cooperking.net>
2018-05-03 08:28:26 +0100

    working on file load

--- lib/roa_renderer/src/importer/gltf.c
+++ lib/roa_renderer/src/importer/gltf.c
@@ -1,11 +1,35 @@
 #include "gltf.h"
-
+#include <json/json.h>
+#include <stdio.h>
 
 
 void
-gltf_import(const char *file, struct gltf_import *out_scene)
+gltf_import(const char *filename, struct gltf_import *out_scene)
 {
+  unsigned char *src = 0;
+  FILE *file = fopen(filename, "r");
 
+  if (file != 0)
+  {
+    if (fseek(file, 0L, SEEK_END) == 0)
+    {
+      long src_size = ftell(file);
+
+      if(src_size == -1) { /* error */ }
+
+      src = malloc(sizeof(src[0]) * (src_size + 1));
+
+      if (fseek(file, 0L, SEEK_SET) != 0)
+      {
+        /* error */  
+      }
+    }
+  }
+
+  CLEANUP:
+  {
+    
+  }
 }
 
 
