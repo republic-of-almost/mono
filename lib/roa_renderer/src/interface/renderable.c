96ad7834355d6a2f712714bf97a8e6e250f65d65
Phil Cooper-King <phil@cooperking.net>
2018-05-12 20:58:18 +0100

    adding mesh renderer data/test

--- lib/roa_renderer/src/interface/renderable.c
+++ lib/roa_renderer/src/interface/renderable.c
@@ -14,7 +14,49 @@ roa_renderer_mesh_renderable_set(
   struct roa_renderer_mesh_renderable *renderable,
   uint32_t renderable_id)
 {
+  /* param check */
+  ROA_ASSERT(ctx);
+  ROA_ASSERT(renderable);
+  ROA_ASSERT(renderable_id);
 
+  ROA_BOOL result = ROA_FALSE;
+
+  if (!ctx || !renderable || !renderable_id)
+  {
+    result = ROA_FALSE;
+    return result;
+  }
+
+  roa_spin_lock_aquire(&ctx->renderer_desc.lock);
+
+  /* search and replace */
+  {
+    uint32_t *ids = ctx->renderer_desc.mesh_rdr_ids;
+    int count = roa_array_size(ctx->renderer_desc.mesh_rdr_ids);
+    int i;
+
+    for (i = 0; i < count; ++i)
+    {
+      if (ids[i] == renderable_id)
+      {
+        result = ROA_TRUE;
+
+        ctx->renderer_desc.mesh_rdr_descs[i] = *renderable;
+
+        roa_spin_lock_release(&ctx->renderer_desc.lock);
+        return result;
+      }
+    }
+  }
+
+  /* insert new one */
+  result = ROA_TRUE;
+
+  roa_array_push(ctx->renderer_desc.mesh_rdr_ids, renderable_id);
+  roa_array_push(ctx->renderer_desc.mesh_rdr_descs, *renderable);
+
+  roa_spin_lock_release(&ctx->renderer_desc.lock);
+  return result;
 }
 
 
@@ -24,7 +66,41 @@ roa_renderer_mesh_renderable_get(
   struct roa_renderer_mesh_renderable *out_renderable,
   uint32_t renderable_id)
 {
- 
+  /* param check */
+  ROA_ASSERT(ctx);
+  ROA_ASSERT(out_renderable);
+  ROA_ASSERT(renderable_id);
+
+  ROA_BOOL result = ROA_FALSE;
+
+  if (!ctx || !out_renderable || !renderable_id)
+  {
+    result = ROA_FALSE;
+    return result;
+  }
+
+  roa_spin_lock_aquire(&ctx->renderer_desc.lock);
+
+  /* search */
+  {
+    uint32_t *ids = ctx->renderer_desc.mesh_rdr_ids;
+    int count = roa_array_size(ctx->renderer_desc.mesh_rdr_descs);
+    int i;
+
+    for (i = 0; i < count; ++i)
+    {
+      if (ids[i] == renderable_id)
+      {
+        result = ROA_TRUE;
+
+        *out_renderable = ctx->renderer_desc.mesh_rdr_descs[i];
+        break;
+      }
+    }
+  }
+
+  roa_spin_lock_release(&ctx->renderer_desc.lock);
+  return result;
 }
 
 
